cac71767686286e7d5b8288eba74f2129cf4468c
Loveseo8 <58880849+Loveseo8@users.noreply.github.com>
2020-12-07 16:16:55 +0300

    +bottomNavigation

--- app/src/main/java/com/add/cipherlearn/MainActivity.java
+++ app/src/main/java/com/add/cipherlearn/MainActivity.java
@@ -17,11 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_learn, R.id.navigation_scoreboard, R.id.navigation_account)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
     }
